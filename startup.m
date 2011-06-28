1f1134db91ba282d6d129e534e5795df6568a5a8
iviorela@gmail.com <iviorela@gmail.com@5168f0d2-b043-b899-3af8-fc9949c0b273>
2011-06-28 14:01:33 +0000

    change startup

--- startup.m
+++ startup.m
@@ -3,11 +3,19 @@
 %   
 clc;
 disp(' ');  
-disp('                          MATLAB TOOLBOX FOR SMOOTHING AND MAPPING');
+disp('                              MATLAB TOOLBOX FOR SMOOTHING AND MAPPING');
 disp(' ');
-disp('              The toolbox implements the SAM and iSAM metrods from the papers:');
-disp('Square Root SAM: SLAM via Square Root Information Smoothing, F. Dellaert and M. Kaess, IJRR, 2006');
-disp('iSAM: Incremental Smoothing and Mapping, M. Kaess, A. Ranganathan, and F. Dellaert, IEEE TRO, 2008 ');
+disp('                   The toolbox compares SLAM oprimization strategies such as:');
+disp('                   a) Incrementaly updating the Cholesky factor [1]     (CholFactor)');
+disp('                   b) Incrementaly updating the Information matrix [2]  (Hessian)');
+disp('                   c) Incrementaly updating the Jacobian matrix [3][4]  (Jacobian)');
+disp(' ');
+disp(' ');
+disp('[1] Sparse Local Submap Joining Filter for Building Large-Scale Maps,S. Huang, Z. Wang and G. Dissanayake, TRO 2008');
+disp('[2] Information-based Compact Pose SLAM, V. Ila, J. M. Porta, and J. Andrade-Cetto, TRO, 2010');
+disp('[3] Square Root SAM: SLAM via Square Root Information Smoothing, F. Dellaert and M. Kaess, IJRR, 2006');
+disp('[4] iSAM: Incremental Smoothing and Mapping, M. Kaess, A. Ranganathan, and F. Dellaert, IEEE TRO, 2008 ');
+
 disp(' ');
 disp('     The toolbox requires Tim Davis SuiteSparse toolbox which can be downloaded from here: ');
 disp('                     http://www.cise.ufl.edu/research/sparse/SuiteSparse/');
