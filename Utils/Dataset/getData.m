f17415ea6e2c137fe8502eac96a3ca885686dbae
iviorela@gmail.com <iviorela@gmail.com@5168f0d2-b043-b899-3af8-fc9949c0b273>
2011-09-25 07:43:49 +0000

    add getdata

---
+++ Utils/Dataset/getData.m
@@ -0,0 +1,17 @@
+function Data=getData(dataSet,pathToolbox,saveFile,maxID)
+
+% getData
+% The script gets data from datafile and returns Data structure
+[vertices,edges]=loadDataSet(dataSet,pathToolbox,saveFile);
+if strcmp(dataSet,'10KHOGMan')
+    % Incremental feed for edges
+    edges=sortrows(edges,[1,-2]); %TODO Change the dataset so that the edges come incrementaly
+end
+[Data.vert, Data.ed]=cut2maxID(maxID, vertices, edges);
+clear vertices edges;
+Data.nVert=size(Data.vert,1);
+Data.nEd=size(Data.ed,1);
+Data.name=dataSet;
+
+
+
