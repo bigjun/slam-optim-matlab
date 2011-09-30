5d6cbe6a95db6fafa438e9b6f8e5f6977b2f1854
iviorela@gmail.com <iviorela@gmail.com@5168f0d2-b043-b899-3af8-fc9949c0b273>
2011-09-30 16:59:52 +0000

    plot landmarks works

--- Utils/Dataset/loadFromFile.m
+++ Utils/Dataset/loadFromFile.m
@@ -1,56 +1,45 @@
 function [vertices, edges]=loadFromFile(dataFileGraph)
-vertices=[];
-edges=[];
+
 fid = fopen(dataFileGraph);
-if isempty(fid)
-    disp('Cannot open the file ');
-else
-    %errorMesage=cat(2,'Reading data from file: ',dataFileGraph,'...');
-    %disp(errorMesage)
-    lines=textscan(fid,'%s','delimiter','\n');
-    fclose(fid);
-    n=size(lines{1},1); % nombre de liniesma
+if isempty(fid) || (fid < 3)
+    error('Cannot open the file %s.\n', dataFileGraph);
+end
     
-    for i=1:n
-        text=lines{1}(i);
-        text=cell2mat(text);
-        line=textscan(text,'%s');
-        if strcmp('VERTEX2',line{1}(1)) || strcmp('VERTEX',line{1}(1))
-            v=[];
-            for j=2:size(line{1},1)
-                v=[v,str2num(cell2mat(line{1}(j)))];
-            end
-            vertices=[vertices;v];
-        elseif strcmp('EDGE2',line{1}(1)) || strcmp('EDGE',line{1}(1))
-            e=[];
-            for j=2:size(line{1},1)
-                e=[e,str2num(cell2mat(line{1}(j)))];
-            end
-            edges=[edges;e];
-        elseif strcmp('ODOMETRY',line{1}(1))
-            odo=[];
-            for j=2:size(line{1},1)
-                odo=[odo,str2num(cell2mat(line{1}(j)))];
-            end
-            edges=[edges;odo];
-        elseif strcmp('LANDMARK',line{1}(1))
-            land=[];
-            for j=2:size(line{1},1)
-                land=[land,str2num(cell2mat(line{1}(j)))];
-            end
-            edges=[edges;land];
-        elseif strcmp('VERTEX3',line{1}(1)) || strcmp('VERTEX',line{1}(1))
-            v=[];
-            for j=2:size(line{1},1)
-                v=[v,str2num(cell2mat(line{1}(j)))];
-            end
-            vertices=[vertices;v];
-        elseif strcmp('EDGE3',line{1}(1)) || strcmp('EDGE',line{1}(1))
-            e=[];
-            for j=2:size(line{1},1)
-                e=[e,str2num(cell2mat(line{1}(j)))];
-            end
-            edges=[edges;e];
-        end
+%errorMesage=cat(2,'Reading data from file: ',dataFileGraph,'...');
+%disp(errorMesage)
+lines = textscan(fid,'%s','delimiter','\n');
+lines = lines{1}; %% to avoid using lines{1} later on
+fclose(fid);
+
+n = size(lines,1); % nombre de liniesma
+vert_strings = {'VERTEX','VERTEX2','VERTEX3'};
+edge_strings = {'EDGE','EDGE2','EDGE3','ODOMETRY','LANDMARK'};
+
+vertex_cell = cell(n,1);
+edge_cell = cell(n,1);
+current_vert = 1;
+current_edge = 1;
+
+for i=1:n
+    text=lines(i);
+    text=cell2mat(text);
+    LINE=textscan(text,'%s'); %% avoid 'line'; it is a matlab builtin
+
+    kind_of_meas = LINE{1}{1};
+    vert_or_edge = str2num(strvcat(LINE{1}{2:end}))';
+    switch kind_of_meas,
+        case vert_strings,
+            vertex_cell{current_vert} = vert_or_edge;
+            current_vert = current_vert + 1;
+
+        case edge_strings,
+            edge_cell{current_edge} = vert_or_edge;
+            current_edge = current_edge + 1;
+
+        otherwise
+            error('Option %s not known.\n', kind_of_meas);
     end
 end
+
+vertices = cat(1, vertex_cell{1:current_vert - 1} );
+edges    = cat(1,   edge_cell{1:current_edge - 1} );
