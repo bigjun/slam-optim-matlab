e82941e45d39093598986ec046a3f8afc53d9de4
iviorela@gmail.com <iviorela@gmail.com@5168f0d2-b043-b899-3af8-fc9949c0b273>
2011-10-10 16:42:00 +0000

    change getGraphFromData

--- Graph/getGraphFromData.m
+++ Graph/getGraphFromData.m
@@ -6,3 +6,18 @@ while ind<=Data.nEd
     Graph=addVarLinkToGraph(factorR,Graph);
     ind=ind+1;
 end
+
+
+%TODO change to not use while:
+
+% % Gets graph from data
+% rows=Data.ed(:,1)+1;
+% cols=Data.ed(:,2)+1;
+% weights = ones(1, size(rows,1));
+% DG = sparse(rows, cols, weights, size(vert,1), size(vert,1));
+% Graph.Matrix = tril(DG + DG');
+%  
+% 
+% %What about:     
+% Graph.idX= [Graph.idX;factorR.data(1)];
+% Graph.F=[Graph.F;factorR];
