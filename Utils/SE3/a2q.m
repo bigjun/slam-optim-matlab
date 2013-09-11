04527bae997f33ec0b39af489bae9924cd2ba68b
iviorela <iviorela@5168f0d2-b043-b899-3af8-fc9949c0b273>
2013-09-11 14:17:19 +0000

    q2a and a2q

---
+++ Utils/SE3/a2q.m
@@ -0,0 +1,15 @@
+function q=axis2quaternion(axis)
+
+% unnormalized axis 
+% q = [w  x  y z]
+thu = sqrt(axis' * axis);
+if thu==0
+    co = 1;
+    si = 0;
+    q = [co; 0; 0; 0];
+else
+    co = cos(thu / 2);
+    si = sin(thu / 2);
+    q = [co; axis * (si / thu)];
+end
+
