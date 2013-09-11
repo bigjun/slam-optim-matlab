67f213ed84b059299382f5834448a3937925789a
iviorela <iviorela@5168f0d2-b043-b899-3af8-fc9949c0b273>
2013-09-11 09:13:52 +0000

    quat replaced functions

--- SEJacobians/utils/ArotMat.m
+++ SEJacobians/utils/ArotMat.m
@@ -1,5 +1,27 @@
 
-function Ln=ArotMat(rd11, rd12, rd13, rd21, rd22, rd23, rd31, rd32, rd33)
+function Ln=ArotMat(varargin)
+if nargin==1
+     rd11=varargin{1}(1,1);
+     rd12=varargin{1}(1,2);
+     rd13=varargin{1}(1,3);
+     rd21=varargin{1}(2,1);
+     rd22=varargin{1}(2,2);
+     rd23=varargin{1}(2,3);
+     rd31=varargin{1}(3,1);
+     rd32=varargin{1}(3,2);
+     rd33=varargin{1}(3,3);
+else
+    rd11=varargin(1);
+     rd12=varargin(2);
+     rd13=varargin(3);
+     rd21=varargin(4);
+     rd22=varargin(5);
+     rd23=varargin(6);
+     rd31=varargin(7);
+     rd32=varargin(8);
+     rd33=varargin(9);
+end
+    
 tr = rd11 + rd22 + rd33;
 cd = (tr-1)/2;
 th = acos(cd);
@@ -10,3 +32,4 @@ r=[rd32 - rd23;
    rd21 - rd12];
 
 Ln=b*r;
+
