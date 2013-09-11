cf95b068faec2efacdba3b2dd9f3587c1575ac38
iviorela <iviorela@5168f0d2-b043-b899-3af8-fc9949c0b273>
2013-09-11 09:11:35 +0000

    quat functions

--- SEJacobians/utils/test_RotArot.m
+++ SEJacobians/utils/test_RotArot.m
@@ -1,29 +1,44 @@
 function test_RotArot
 uR = [ 0.341895;  -0.041700;  0.033039;  -0.003792;  0.007925;  0.180211];
 vR = [ 0.541643;  0.135006;  -0.067787;  -0.007457;  0.024664;  0.291557];
-Ru=RotMat(uR(4:6)) %WRONG!!!
-Qu = rot(uR(4:6))
-Rv=RotMat(vR(4:6))%WRONG!!!
-Qv = rot(vR(4:6))
+Ru=RotMat(uR(4:6)) ;
+Qu = rot(uR(4:6));
+qu = a2q(uR(4:6));
+
+
+Rv=RotMat(vR(4:6));
+Qv = rot(vR(4:6));
+qv = a2q(vR(4:6));
 
 tu = uR(1:3);
 tv = vR(1:3);
 
+%A2R rotation
 tdR = Ru'*(tv - tu); 
-RdR= Ru'*Rv;
+RdR = Ru'*Rv; 
+dR=[tdR; arot(RdR)]
 
+%A2R quats
+tdq = quatrotate(quatconj(qu'),(tv - tu)'); 
+Rdq = quatmultiply(quatconj(qu'),qv');
 
- cd = (trace(RdR)-1)/2;
- sd = sqrt(1 - cd^2);
- angle = acos(cd);
- b=angle/(2*sd);
- DR=[RdR(3,2) - RdR(2,3); RdR(1,3) - RdR(3,1); RdR(2,1) - RdR(1,2)]* ((angle*cd-sd)/(4*sd^3))
- 
- 
+dq = [tdq';q2a(Rdq')]
 
-dR = arot (RdR)
-d=[tdR; dR]
 
 d_gt = [ 0.229005;  0.138346;  -0.098524;  -0.002594;  0.016774;  0.111368]
+
+% dR-d_gt
+% dq-d_gt
+% 
+ Rdq_gt = a2q(d_gt(4:6));
+% Rdq'- Rdq_gt
+% tdq'- d_gt(1:3)
+
+
+vp = tu'+ quatrotate(qu',d_gt(1:3)')
+vp-tv'
+vr = quatmultiply(qu', Rdq_gt')
+vr-qv'
+
 % difference between u and v
 
