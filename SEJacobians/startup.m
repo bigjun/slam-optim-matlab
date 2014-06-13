c04cca82aa4e96af6285f4fc815ebc5a50f24755
iviorela <iviorela@5168f0d2-b043-b899-3af8-fc9949c0b273>
2014-06-13 15:24:57 +0000

    startup

---
+++ SEJacobians/startup.m
@@ -0,0 +1,14 @@
+% Matlab toolbox for smoothing and mapping
+%
+%   
+clc;
+disp(' ');  
+disp('                                    MATLAB TOOLBOX FOR SE(3) JACOBIANS');
+disp(' ');
+disp(' ');
+disp(' ');
+disp('                                                 ENJOY!!');
+disp(' ');
+disp('                                          Author: Viorela Ila');
+  
+path(path,genpath(pwd));
